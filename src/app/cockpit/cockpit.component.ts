393c3e9b8f1fab1290f856997330953025a4f8d0
rafael.michelli <rafael.michelli@mprj.mp.br>
2017-07-27 18:08:50 +0000

    Adding local reference by an attribute in HTML.

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -17,18 +17,20 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     console.log('Call: CockpitComponent -> onAddServer');
     console.log('serverCreated: ' + this.serverCreated);
     this.serverCreated.emit({
-      serverName: this.newServerName,
+     /* serverName: this.newServerName,*/
+     serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
+/*      serverName: this.newServerName,*/
+      serverName: nameInput.value,
       serverContent: this.newServerContent
     });
   }
